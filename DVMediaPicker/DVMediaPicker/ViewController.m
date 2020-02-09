6f0b2517f49b26961735ebe8794f80a8e17bbd93
david <337581468@qq.com>
2020-02-09 13:41:06 +0800

    适配iOS13的presentViewController，增加modalPresentationStyle

--- DVMediaPicker/DVMediaPicker/ViewController.m
+++ DVMediaPicker/DVMediaPicker/ViewController.m
@@ -17,13 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        
         DVMediaPickerContoller * vc = [[DVMediaPickerContoller alloc] init];
+        vc.modalPresentationStyle = UIModalPresentationFullScreen;
         [self presentViewController:vc animated:true completion:nil];
     });
-  
     // Do any additional setup after loading the view, typically from a nib.
 }
 
